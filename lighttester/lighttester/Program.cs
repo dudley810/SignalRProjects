85e497630d1dc72d289ff0b387a5014e9141a129
dudley.kenneth <kenneth.dudley@level3.com>
2014-12-04 22:37:01 -0500

    testing

---
+++ lighttester/lighttester/Program.cs
@@ -0,0 +1,24 @@
+﻿using RaspberryPiDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lighttester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GPIOMem gpio01 = new GPIOMem(GPIOPins.GPIO_01);
+
+            for (int i = 0; i < 100; i++)
+            {
+                PinState ps = gpio01.Read();
+                Console.WriteLine(ps.ToString());
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
